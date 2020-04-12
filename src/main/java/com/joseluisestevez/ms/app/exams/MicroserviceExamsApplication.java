59aaf1eae6d41ba338db581485fd2511eab7ead5
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-12 09:22:24 +0200

    New SpringBoot project - Exams

---
+++ src/main/java/com/joseluisestevez/ms/app/exams/MicroserviceExamsApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.app.exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceExamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceExamsApplication.class, args);
+    }
+
+}
