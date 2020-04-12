4ff0ee3040e01ead8cac0744fad628e42a16124d
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-12 10:22:08 +0200

    Adding bidirectional relationship in exam with questions

--- src/main/java/com/joseluisestevez/ms/app/exams/models/entity/Exam.java
+++ src/main/java/com/joseluisestevez/ms/app/exams/models/entity/Exam.java
@@ -1,24 +1,35 @@
 package com.joseluisestevez.ms.app.exams.models.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "exams")
 public class Exam {
@@ -32,9 +43,28 @@ public class Exam {
     @Column(name = "create_at")
     private Date createAt;
 
+    @JsonIgnoreProperties(value = { "exam" }, allowSetters = true)
+    @OneToMany(mappedBy = "exam", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> questions = new ArrayList<>();
+
     @PrePersist
     public void prePersist() {
         this.createAt = new Date();
+    }
+
+    public void setQuestions(Iterable<Question> questions) {
+        this.questions.clear();
+        questions.forEach(this::addQuestion);
+    }
+
+    public void addQuestion(Question question) {
+        this.questions.add(question);
+        question.setExam(this);
+    }
+
+    public void remiveQuestion(Question question) {
+        this.questions.remove(question);
+        question.setExam(null);
     }
 
 }
--- src/main/java/com/joseluisestevez/ms/app/exams/models/entity/Question.java
+++ src/main/java/com/joseluisestevez/ms/app/exams/models/entity/Question.java
@@ -1,18 +1,27 @@
 package com.joseluisestevez.ms.app.exams.models.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "questions")
 public class Question {
@@ -20,5 +29,23 @@ public class Question {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String text;
+
+    @JsonIgnoreProperties(value = { "questions" })
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exam_id")
+    private Exam exam;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Question)) {
+            return false;
+        }
+        Question a = (Question) o;
+
+        return this.id != null && this.id.equals(a.getId());
+    }
 
 }
