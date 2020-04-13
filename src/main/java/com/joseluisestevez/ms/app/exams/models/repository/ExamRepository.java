d6f0d2a7b59af0e16a3ca43b6ca36fc686c66fdc
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-13 11:07:44 +0200

    Add pagination

--- src/main/java/com/joseluisestevez/ms/app/exams/models/repository/ExamRepository.java
+++ src/main/java/com/joseluisestevez/ms/app/exams/models/repository/ExamRepository.java
@@ -3,11 +3,11 @@ package com.joseluisestevez.ms.app.exams.models.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.joseluisestevez.ms.commons.exams.models.entity.Exam;
 
-public interface ExamRepository extends CrudRepository<Exam, Long> {
+public interface ExamRepository extends PagingAndSortingRepository<Exam, Long> {
 
     @Query("select e from Exam e where e.name like %?1%")
     List<Exam> findByName(String name);
